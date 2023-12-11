f81e2c6f31fc526c60e0d2d68f9124886168b51b
Jakub Jadczak <jakubjadczak02@aoutlook.com>
2023-12-12 00:47:22 +0100

    Short & Extend Shortcuts without case

--- src/main/java/pl/put/poznan/transformer/logic/ShortExtender.java
+++ src/main/java/pl/put/poznan/transformer/logic/ShortExtender.java
@@ -1,9 +1,10 @@
 package pl.put.poznan.transformer.logic;
+
 import java.util.HashMap;
 
-public class ShortCutter {
+public class ShortExtender {
     /*
-    Function shortens selected phrases
+    Function extends shortcuts, provided by user
     Phrases and shortcuts are stored using HashMap
     Parameters:
         input(String) text provided by user
@@ -14,12 +15,12 @@ public class ShortCutter {
     private final HashMap<String, String> shortcutsDict = new HashMap<>();
 
     private void setup(){
-        shortcutsDict.put("na przykład", "np.");
-        shortcutsDict.put("między innymi", "m.in.");
-        shortcutsDict.put("i tym podobne", "itd.");
-        shortcutsDict.put("profesor", "prof.");
-        shortcutsDict.put("doktor", "dr");
-        shortcutsDict.put("magister", "mgr");
+        shortcutsDict.put("np.", "na przykład");
+        shortcutsDict.put("m.in.", "między innymi");
+        shortcutsDict.put("itd.", "i tym podobne");
+        shortcutsDict.put("prof.", "profesor");
+        shortcutsDict.put("dr", "doktor");
+        shortcutsDict.put("mgr", "magister");
     }
 
     public String transform(String input){
@@ -33,5 +34,4 @@ public class ShortCutter {
         }
         return input;
     }
-
 }
