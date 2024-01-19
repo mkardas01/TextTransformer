bf3463a152e7e5102c2b42aaeef0b7da301a847d
Jakub Jadczak <jakubjadczak02@aoutlook.com>
2024-01-19 19:27:24 +0100

    MockObjects, 5 classes

--- src/test/java/pl/put/poznan/transformer/logic/TextTransformerTest.java
+++ src/test/java/pl/put/poznan/transformer/logic/TextTransformerTest.java
@@ -2,9 +2,7 @@ package pl.put.poznan.transformer.logic;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import pl.put.poznan.transformer.logic.decorators.CapitalizeDecorator;
-import pl.put.poznan.transformer.logic.decorators.LowerDecorator;
-import pl.put.poznan.transformer.logic.decorators.UpperDecorator;
+import pl.put.poznan.transformer.logic.decorators.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -18,31 +16,55 @@ public class TextTransformerTest {
     private TextTransformation lowerDecorator;
     private TextTransformation capitalizeDecorator;
 
+    private TextTransformation inverseDecorator;
+    private TextTransformation shortCutExtenderDecorator;
+
     @BeforeEach
-    void setUp() throws NoSuchFieldException, IllegalAccessException {
+    void setUp(){
         upperDecorator = mock(UpperDecorator.class);
         lowerDecorator = mock(LowerDecorator.class);
         capitalizeDecorator = mock(CapitalizeDecorator.class);
+        inverseDecorator = mock(InverseDecorator.class);
+        shortCutExtenderDecorator = mock(ShortExtenderDecorator.class);
+    }
 
-        String[] transforms = {"upper", "lower", "capitalize"};
+    @Test
+    void testTransform() throws IllegalAccessException, NoSuchFieldException {
+        String[] transforms = {"upper", "lower", "capitalize", "inverse"};
         textTransformer = new TextTransformer(transforms);
 
         Field transformersField = TextTransformer.class.getDeclaredField("transformers");
         transformersField.setAccessible(true);
-        transformersField.set(textTransformer, new TextTransformation[] {upperDecorator, lowerDecorator, capitalizeDecorator});
-    }
+        transformersField.set(textTransformer, new TextTransformation[] {upperDecorator, lowerDecorator,
+                capitalizeDecorator, inverseDecorator});
 
-    @Test
-    void testTransform() {
         when(upperDecorator.transform("test")).thenReturn("TEST");
         when(lowerDecorator.transform("TEST")).thenReturn("test");
         when(capitalizeDecorator.transform("test")).thenReturn("Test");
+        when(inverseDecorator.transform("Test")).thenReturn("tseT");
 
         String result = textTransformer.transform("test");
-        assertEquals("Test", result);
+        assertEquals("tseT", result);
 
         verify(upperDecorator).transform("test");
         verify(lowerDecorator).transform("TEST");
         verify(capitalizeDecorator).transform("test");
+        verify(inverseDecorator).transform("Test");
+    }
+    @Test
+    void testTransform1() throws IllegalAccessException, NoSuchFieldException {
+        String[] transforms = {"extend"};
+        textTransformer = new TextTransformer(transforms);
+
+        Field transformersField = TextTransformer.class.getDeclaredField("transformers");
+        transformersField.setAccessible(true);
+        transformersField.set(textTransformer, new TextTransformation[] {shortCutExtenderDecorator});
+
+        when(shortCutExtenderDecorator.transform("np.")).thenReturn("na przykład");
+
+        String result = textTransformer.transform("np.");
+        assertEquals("na przykład", result);
+
+        verify(shortCutExtenderDecorator).transform("np.");
     }
 }
